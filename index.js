892904833a1bf0b2f8530614ce1e7dceae90a4fa
herrerasaurus <blue777ysk@gmail.com>
2023-04-12 23:19:34 +0900

    firtst commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const { createProxyMiddleware } = require("http-proxy-middleware")
+const rateLimit = require("express-rate-limit")
+require("dotenv").config()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10000,
+})
+
+
+app.get("/", (req, res) => {
+    res.send("This is my proxy server")
+})
+
+app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
+    createProxyMiddleware({
+        target: process.env.BASE_API_URL_CORONA_WORLD,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/corona-tracker-world-data`]: "",
+        },
+    }) (req, res, next)
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Listening on localhost port 5000 ${port}`)
+})
+
+module.exports = app
